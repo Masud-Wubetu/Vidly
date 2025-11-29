36a98ef4232eaa1f8f42af10c3f5fd2bc85e4525
Masud <wubetumasud@gmail.com>
2025-11-29 09:33:28 +0300

    Handling Post Http request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const genres = [
     {id: 1, name: 'Action'},
     {id: 2, name: 'Horror'},
@@ -12,7 +14,21 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 });
 
-function validateCourse(genre){
+app.post('/api/genres', (req, res) => {
+    const {error} = validateGenre(req.body);
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+    genres.push(genre);
+    res.send(genre);
+})
+
+function validateGenre(genre){
    const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
